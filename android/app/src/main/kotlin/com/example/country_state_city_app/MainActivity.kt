910013b0355d1f06846a606d4a6a8bfe69219f90
JatinderV <JatinderV@cidom.careington.com>
2023-09-21 10:28:25 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/country_state_city_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.country_state_city_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:country_state_city_app/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Country State City',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
